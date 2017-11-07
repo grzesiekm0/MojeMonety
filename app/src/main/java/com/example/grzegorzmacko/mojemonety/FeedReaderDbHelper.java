6e403e9d5eca44ccd9c7d3705c8efb0e1d8bbbdd
grzesiekm0 <grzmacko@gmail.com>
2017-11-07 18:24:52 +0100

    Refactoring code and adding comments.

--- app/src/main/java/com/example/grzegorzmacko/mojemonety/FeedReaderDbHelper.java
+++ app/src/main/java/com/example/grzegorzmacko/mojemonety/FeedReaderDbHelper.java
@@ -16,10 +16,11 @@ import java.util.ArrayList;
 public class FeedReaderDbHelper extends SQLiteOpenHelper {
 
 
-
+    // Global variables to version and name
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "Kolekcja monet.db";
 
+    // Columns names to the database
     private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + FeedReaderContract.FeedEntry.TABLE_NAME + " (" +
                     FeedReaderContract.FeedEntry._ID + " INTEGER PRIMARY KEY autoincrement ," +
@@ -33,17 +34,18 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
                     FeedReaderContract.FeedEntry.COLUMN_NAME_WAGA + " INTEGER," +
                     FeedReaderContract.FeedEntry.COLUMN_NAME_SREDNICA + " INTEGER," +
                     FeedReaderContract.FeedEntry.COLUMN_NAME_WARTOSC + " INTEGER )";
-
+    //Drop table
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + FeedReaderContract.FeedEntry.TABLE_NAME;
 
     Context context;
+
     public FeedReaderDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
     }
 
-
+    // Create database
     @Override
     public void onCreate(SQLiteDatabase db) {
 
@@ -57,33 +59,30 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-
-
-
-
-
-    //Metoda dodajaca monete do bazy danych
+    /**
+     * //This method add a coin to the database
+     */
     public boolean DodajMonetee(String waluta, Integer rok, String ksztalt, String metal,
-                               String stop_metalu, String kraj_pochodzenia, Integer nominal, Integer waga, Integer srednica, Integer wartosc) {
+                                String stop_metalu, String kraj_pochodzenia, Integer nominal, Integer waga, Integer srednica, Integer wartosc) {
 
+        //Create and/or open a database that will be used for reading and writing
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues value = new ContentValues();
-        // String rok_con;
-        //rok_con = Integer.toString(rok);
 
-        //String.valueOf(rok);
-        value.put( FeedReaderContract.FeedEntry.COLUMN_NAME_WALUTA, waluta);
-        value.put( FeedReaderContract.FeedEntry.COLUMN_NAME_ROK, rok);
-        value.put( FeedReaderContract.FeedEntry.COLUMN_NAME_KSZTALT, ksztalt);
-        value.put( FeedReaderContract.FeedEntry.COLUMN_NAME_METAL, metal);
-        value.put( FeedReaderContract.FeedEntry.COLUMN_NAME_STOP_METALU, stop_metalu);
-        value.put( FeedReaderContract.FeedEntry.COLUMN_NAME_KRAJ_POCHODZENIA, kraj_pochodzenia);
-        value.put( FeedReaderContract.FeedEntry.COLUMN_NAME_NOMINAL, nominal);
-        value.put( FeedReaderContract.FeedEntry.COLUMN_NAME_WAGA, waga);
-        value.put( FeedReaderContract.FeedEntry.COLUMN_NAME_SREDNICA, srednica);
-        value.put( FeedReaderContract.FeedEntry.COLUMN_NAME_WARTOSC, wartosc);
+        value.put(FeedReaderContract.FeedEntry.COLUMN_NAME_WALUTA, waluta);
+        value.put(FeedReaderContract.FeedEntry.COLUMN_NAME_ROK, rok);
+        value.put(FeedReaderContract.FeedEntry.COLUMN_NAME_KSZTALT, ksztalt);
+        value.put(FeedReaderContract.FeedEntry.COLUMN_NAME_METAL, metal);
+        value.put(FeedReaderContract.FeedEntry.COLUMN_NAME_STOP_METALU, stop_metalu);
+        value.put(FeedReaderContract.FeedEntry.COLUMN_NAME_KRAJ_POCHODZENIA, kraj_pochodzenia);
+        value.put(FeedReaderContract.FeedEntry.COLUMN_NAME_NOMINAL, nominal);
+        value.put(FeedReaderContract.FeedEntry.COLUMN_NAME_WAGA, waga);
+        value.put(FeedReaderContract.FeedEntry.COLUMN_NAME_SREDNICA, srednica);
+        value.put(FeedReaderContract.FeedEntry.COLUMN_NAME_WARTOSC, wartosc);
 
+        // Insert to database
         long newRowId = db.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, value);
+
         if (newRowId == -1) {
             return false;
         } else
@@ -91,32 +90,30 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
 
     }
 
-
-    //Metoda dodajaca monete do bazy danych
+    /**
+     * //This method update a coin to the database
+     */
     public boolean AktualizujMonete(int idd, String waluta, Integer rok, String ksztalt, String metal,
-                                String stop_metalu, String kraj_pochodzenia, Integer nominal, Integer waga, Integer srednica, Integer wartosc) {
+                                    String stop_metalu, String kraj_pochodzenia, Integer nominal, Integer waga, Integer srednica, Integer wartosc) {
 
+        //Create and/or open a database that will be used for reading and writing
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues valuee = new ContentValues();
-        // String rok_con;
-        //rok_con = Integer.toString(rok);
 
-        //String.valueOf(rok);
-        valuee.put( FeedReaderContract.FeedEntry.COLUMN_NAME_WALUTA, waluta);
-        valuee.put( FeedReaderContract.FeedEntry.COLUMN_NAME_ROK, rok);
-        valuee.put( FeedReaderContract.FeedEntry.COLUMN_NAME_KSZTALT, ksztalt);
-        valuee.put( FeedReaderContract.FeedEntry.COLUMN_NAME_METAL, metal);
-        valuee.put( FeedReaderContract.FeedEntry.COLUMN_NAME_STOP_METALU, stop_metalu);
-        valuee.put( FeedReaderContract.FeedEntry.COLUMN_NAME_KRAJ_POCHODZENIA, kraj_pochodzenia);
-        valuee.put( FeedReaderContract.FeedEntry.COLUMN_NAME_NOMINAL, nominal);
-        valuee.put( FeedReaderContract.FeedEntry.COLUMN_NAME_WAGA, waga);
-        valuee.put( FeedReaderContract.FeedEntry.COLUMN_NAME_SREDNICA, srednica);
-        valuee.put( FeedReaderContract.FeedEntry.COLUMN_NAME_WARTOSC, wartosc);
+        valuee.put(FeedReaderContract.FeedEntry.COLUMN_NAME_WALUTA, waluta);
+        valuee.put(FeedReaderContract.FeedEntry.COLUMN_NAME_ROK, rok);
+        valuee.put(FeedReaderContract.FeedEntry.COLUMN_NAME_KSZTALT, ksztalt);
+        valuee.put(FeedReaderContract.FeedEntry.COLUMN_NAME_METAL, metal);
+        valuee.put(FeedReaderContract.FeedEntry.COLUMN_NAME_STOP_METALU, stop_metalu);
+        valuee.put(FeedReaderContract.FeedEntry.COLUMN_NAME_KRAJ_POCHODZENIA, kraj_pochodzenia);
+        valuee.put(FeedReaderContract.FeedEntry.COLUMN_NAME_NOMINAL, nominal);
+        valuee.put(FeedReaderContract.FeedEntry.COLUMN_NAME_WAGA, waga);
+        valuee.put(FeedReaderContract.FeedEntry.COLUMN_NAME_SREDNICA, srednica);
+        valuee.put(FeedReaderContract.FeedEntry.COLUMN_NAME_WARTOSC, wartosc);
 
-
-
+        // Update to database
         long newRowId = db.update(FeedReaderContract.FeedEntry.TABLE_NAME, valuee, FeedReaderContract.FeedEntry
-        ._ID+"="+idd,null);
+                ._ID + "=" + idd, null);
         if (newRowId == -1) {
             db.close();
             return false;
@@ -127,7 +124,6 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
     }
 
 
-
     /**
      * //This method returns all coins from the database
      */
@@ -135,15 +131,15 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getReadableDatabase();
         ArrayList<moneta> listItems = new ArrayList<moneta>();
 
+        //Get results from the database to Cursor
         Cursor cursor = db.rawQuery("SELECT * from " + FeedReaderContract.FeedEntry.TABLE_NAME,
-                new String[] {});
+                new String[]{});
 
         if (cursor.moveToFirst()) {
             do {
                 moneta mon = new moneta();
 
                 mon._id = cursor.getInt(cursor.getColumnIndex(FeedReaderContract.FeedEntry._ID));
-
                 mon.waluta = cursor.getString(cursor.getColumnIndex(FeedReaderContract.FeedEntry.COLUMN_NAME_WALUTA));
                 mon.rok = cursor.getInt(cursor.getColumnIndex(FeedReaderContract.FeedEntry.COLUMN_NAME_ROK));
                 mon.ksztalt = cursor.getString(cursor.getColumnIndex(FeedReaderContract.FeedEntry.COLUMN_NAME_KSZTALT));
@@ -155,10 +151,11 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
                 mon.srednica = cursor.getInt(cursor.getColumnIndex(FeedReaderContract.FeedEntry.COLUMN_NAME_SREDNICA));
                 mon.wartosc = cursor.getInt(cursor.getColumnIndex(FeedReaderContract.FeedEntry.COLUMN_NAME_WARTOSC));
 
+                // Adding object to ListItem
                 listItems.add(mon);
             } while (cursor.moveToNext());
         }
-        // trzeba zamknac baze danych bo kosztuje duzo pamieci i czasu
+        // Closing databases because it consumes a lot of memory and time
         db.close();
         cursor.close();
 
@@ -166,40 +163,18 @@ public class FeedReaderDbHelper extends SQLiteOpenHelper {
     }
 
 
-    // Metoda z typem Cursor wyswietlajaca wyniki bazy sqlite
-    public Cursor wyswietlWszystkieRekordyy() {
-        String where = null;
-        String[] kolumny = {"_id", "waluta", "rok", "ksztalt", "metal", "stop_metalu", "kraj_pochodzenia", "nominal", "waga", "srednica", "wartosc"};
+    /**
+     * //This method removing a coin from the database
+     */
+    public void removee(long id) {
+
+        //Create and/or open a database.
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor kursor = db.query("moneta", kolumny, null, null, null, null, null);
-        if(kursor != null){
-            kursor.moveToFirst();
-        }
-        return kursor;
-    }
-
-
-    public void removee(long id){
-        SQLiteDatabase db = this.getReadableDatabase();
-        String string =String.valueOf(id);
-        // db.execSQL("DELETE FROM moneta WHERE _id = '" + string + "'");
-        db.execSQL("DELETE FROM " + "moneta" + " WHERE " + "_id" + "= '" + string + "'");
-
-
-    }
-
-    public void deleteNote(long id) {
-        SQLiteDatabase db = this.getReadableDatabase();
-
         String string = String.valueOf(id);
-        db.execSQL("DELETE FROM " + FeedReaderContract.FeedEntry.TABLE_NAME + " WHERE " + FeedReaderContract.FeedEntry._ID
-                + "=" + id + "");
+        db.execSQL("DELETE FROM " + FeedReaderContract.FeedEntry.TABLE_NAME + " WHERE " + FeedReaderContract.FeedEntry._ID + "= '" + string + "'");
+
+
     }
-
-
-
-
-
 
 
 }
