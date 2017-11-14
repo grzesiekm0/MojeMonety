bcdd0bf236ff1e8d77f8e959e53a17cb25fbcf24
grzesiekm0 <grzmacko@gmail.com>
2017-11-14 17:12:45 +0100

    New activity AddCoinActivity and editing activity_dodaj_monete and activity_item

---
+++ app/src/main/java/com/example/grzegorzmacko/mojemonety/AddCoinActivity.java
@@ -0,0 +1,13 @@
+package com.example.grzegorzmacko.mojemonety;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_coin);
+    }
+}
